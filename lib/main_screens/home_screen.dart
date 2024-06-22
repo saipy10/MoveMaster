244b7a771cee897385abd2ea97f1827179c1a9bd
saipy10 <saipy.code@gmail.com>
2024-06-22 22:00:09 +0530

    Initial Commit

---
+++ lib/main_screens/home_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chess/helper/helper_methods.dart';
+import 'package:flutter_chess/main_screens/about_screen.dart';
+import 'package:flutter_chess/main_screens/game_time_screen.dart';
+import 'package:flutter_chess/main_screens/settings_screen.dart';
+import 'package:flutter_chess/providers/game_provider.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final gameProvider = context.read<GameProvider>();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.brown,
+        title: const Text(
+          'MoveMaster',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.brown.shade100,
+              Colors.brown.shade300,
+              Colors.brown.shade500,
+            ],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 1.0),
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.count(
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              children: [
+                buildGameType(
+                  lable: 'Play vs Computer',
+                  icon: Icons.computer,
+                  onTap: () {
+                    gameProvider.setVsComputer(value: true);
+                    // navigate to setup game time screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const GameTimeScreen(),
+                      ),
+                    );
+                  },
+                ),
+                buildGameType(
+                  lable: 'Play vs Friend',
+                  icon: Icons.person,
+                  onTap: () {
+                    gameProvider.setVsComputer(value: false);
+                    // navigate to setup game time screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const GameTimeScreen(),
+                      ),
+                    );
+                  },
+                ),
+                buildGameType(
+                  lable: 'Settings',
+                  icon: Icons.settings,
+                  onTap: () {
+                    // navigate to settings screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SettingsScreen(),
+                      ),
+                    );
+                  },
+                ),
+                buildGameType(
+                  lable: 'About',
+                  icon: Icons.info,
+                  onTap: () {
+                    // navigate to about screen
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AboutScreen(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/service/assets_manager.dart
@@ -0,0 +1,9 @@
+class AssetsManager {
+  static String Images = "assets/images";
+
+  static String userIcon = "$Images/profile.png";
+  static String chessIcon = "$Images/chess.png";
+  static String googleIcon = "$Images/google.png";
+  static String stockfishIcon = "$Images/stockfish.png";
+  static String facebook = "$Images/facebook.png";
+}
